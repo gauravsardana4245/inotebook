fe76133b2784dc9d98703e4a405a507ab1c39df4
gauravsardana4245 <gaurravsarrdana@gmail.com>
2023-03-12 15:04:05 +0530

    Initial commit

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
 var cors = require('cors')
 const connectToMongo = require("./db.js");
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', "https://inotebook-gaurav.netlify.app/");
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 app.use(cors())
 app.use('/api/auth', require('/Users/gauravsardana/inotebook/Backend/routes/auth.js'))
 app.use('/api/notes', require('/Users/gauravsardana/inotebook/Backend/routes/notes.js'))
